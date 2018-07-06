c108fb90a63efda51301ce9fb75e7d67d19c1b41
Vielbert <vielfade@yahoo.com>
2018-07-06 19:21:21 +0700

    uts

---
+++ sumx/sumx/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumx
+{
+    class Program
+    {
+        
+        public static int sumX(int x, int n) {
+            int a , b ;
+            int hasil;
+            hasil = 0;
+            a = x;
+            b = n;
+            if (a > 0 && b > 0)
+            {
+                
+                    for (int i = 1; i <= b; i++)
+                    {
+                        if ((i) % (a) == 0)
+                        {
+                            hasil = i + hasil;
+                            
+                        }
+                        
+                        else
+                        {
+                            
+                        }
+                    }
+                
+            }
+            else if (a < 0 && b < 0)
+            {
+               a= a * -1;
+                b = b * -1;
+
+                    for (int i = 1; i <= b; i++)
+                    {
+                        if ((i) % (a) == 0)
+                        {
+                        hasil = i + hasil;
+                      
+                            
+                        }
+                        else
+                        {
+
+                          
+                        }
+                    
+                }hasil = hasil * -1;
+            }
+           else if (a < 0 && b > 0 || a > 0 && b < 0 || a == 0 && (b > 0 || b < 0) || (a > 0 || a < 0) && b == 0)
+            {
+                hasil = 0;
+            }
+            return hasil;
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine((sumX(-1,-10)));
+            Console.WriteLine("sumX(3, 10): " + (sumX(3, 10) == 18));
+            Console.WriteLine("sumX(1, 10): " + (sumX(1, 10) == 55));
+            Console.WriteLine("sumX(0, 10): " + (sumX(0, 10) == 0));
+            Console.WriteLine("sumX(-1, 10): " + (sumX(-1, 10) == 0));
+            Console.WriteLine("sumX(-1, -10): " + (sumX(-1, -10) == -55));
+        }
+    }
+}
